556c5439ddcb3fe327633618f1919eda6159494d
joy <sh940311@gmail.com>
2022-11-03 22:01:45 +0900

    로그인 jsp

--- src/main/java/com/douzone/blah/controller/UserController.java
+++ src/main/java/com/douzone/blah/controller/UserController.java
@@ -2,12 +2,19 @@ package com.douzone.blah.controller;
 
 import java.util.HashMap;
 import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.douzone.blah.dao.User2DAO;
 import com.douzone.blah.model.User2DTO;
+
+import lombok.extern.log4j.Log4j;
+
 
 @Controller
 public class UserController {
@@ -17,11 +24,8 @@ public class UserController {
 
   @RequestMapping("/admin")
   public String adminHandler(HttpServletRequest request) {
-
-
       return "admin/admin";
   }
-
 
   @RequestMapping("/admin/edit")
   public String list(HttpServletRequest request) {
@@ -43,5 +47,11 @@ public class UserController {
     }
     return res;
   }
+  
+  @GetMapping("/login/login")
+  public void logIn() {
+	  System.out.println("call logIn()");
+  }
+  
 
 }
