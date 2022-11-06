3a7dc88bd148a5da9c7a23d6f0a0b901fc3cee71
Jmongle <110084761+Jmongle@users.noreply.github.com>
2022-11-06 19:47:11 +0900

    주말기능구현(업데이트, 조회수, 댓글)

---
+++ src/main/java/com/douzone/blah/dao/PostReviewDAO.java
@@ -0,0 +1,15 @@
+package com.douzone.blah.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.douzone.blah.model.PostReviewDTO;
+
+public interface PostReviewDAO {
+
+	//댓글 조회
+	public List<PostReviewDTO> getPostReview(int post_num);
+	
+	//댓글 작성
+	public void insertPostReview(PostReviewDTO dto);
+}
